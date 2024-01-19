e429e8c837197311df092761f68878573fba0bae
Lei Li <lei.xw.li@hotmail.com>
2024-01-18 19:56:17 -0800

    done including  bonus 1

--- frontend/src/components/PawsRatingInput.jsx
+++ frontend/src/components/PawsRatingInput.jsx
@@ -1,32 +1,54 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
   const [activeRating, setActiveRating] = useState(rating)
-  console.log("disabled is", disabled)
+
+  useEffect(() => {
+    setActiveRating(rating);
+  }, [rating])
+
+  function reviewCreator (num) {
+    return (
+      <div className={activeRating >= num ? "filled" : "empty"} onMouseEnter={()=>{if(!disabled) setActiveRating(num)}} onMouseLeave={() => {if(!disabled)setActiveRating(rating)}} onClick={() => onChange(num)}>
+          <i className="fa fa-paw"></i>
+        </div>
+    )
+  }
+
   return (
     <>
-      <input
-        type="number"
-        disabled={disabled}
-        value={activeRating}
-        onChange={onChange}
-      />
       <div className="rating-input">
-        <div className={activeRating >= 1 ? "filled" : "empty"} onMouseEnter={()=>{if(!disabled) setActiveRating(1)}} onMouseLeave={() => {if(!disabled) setActiveRating(rating)}}>
+        {
+         reviewCreator(1)
+        }
+        {
+         reviewCreator(2)
+        }
+        {
+         reviewCreator(3)
+        }
+        {
+         reviewCreator(4)
+        }
+        {
+         reviewCreator(5)
+        }
+        
+       { /* <div className={activeRating >= 1 ? "filled" : "empty"} onMouseEnter={()=>{if(!disabled) setActiveRating(1)}} onMouseLeave={() => {if(!disabled)setActiveRating(rating)}} onClick={() => onChange(1)}>
           <i className="fa fa-paw"></i>
         </div>
-        <div className={activeRating >= 2 ? "filled" : "empty"} onMouseEnter={()=>setActiveRating(2)} onMouseLeave={() => setActiveRating(rating)}>
+        <div className={activeRating >= 2 ? "filled" : "empty"} onMouseEnter={()=>{if(!disabled)setActiveRating(2)}} onMouseLeave={() => {if(!disabled) setActiveRating(rating)}} onClick={() => onChange(2)}>
           <i className="fa fa-paw"></i>
         </div>
-        <div className={activeRating >= 3 ? "filled" : "empty"} onMouseEnter={()=>setActiveRating(3)} onMouseLeave={() => setActiveRating(rating)}>
+        <div className={activeRating >= 3 ? "filled" : "empty"} onMouseEnter={()=>{if(!disabled) setActiveRating(3)}} onMouseLeave={() => {if(!disabled)setActiveRating(rating)}} onClick={() => onChange(3)}>
           <i className="fa fa-paw"></i>
         </div>
-        <div className={activeRating >= 4 ? "filled" : "empty"} onMouseEnter={()=>setActiveRating(4)} onMouseLeave={() => setActiveRating(rating)}>
+        <div className={activeRating >= 4 ? "filled" : "empty"} onMouseEnter={()=>{if(!disabled) setActiveRating(4)}} onMouseLeave={() => {if(!disabled)setActiveRating(rating)}} onClick={() => onChange(4)}>
           <i className="fa fa-paw"></i>
         </div>
-        <div className={activeRating >= 5 ? "filled" : "empty"} onMouseEnter={()=>setActiveRating(5)} onMouseLeave={() => setActiveRating(rating)}>
+        <div className={activeRating >= 5 ? "filled" : "empty"} onMouseEnter={()=>{if(!disabled) setActiveRating(5)}} onMouseLeave={() => {if(!disabled)setActiveRating(rating)}} onClick={() => onChange(5)}>
           <i className="fa fa-paw"></i>
-        </div>
+        </div> */}
       </div>
     </>
   );
--- frontend/src/components/ReviewItem.jsx
+++ frontend/src/components/ReviewItem.jsx
@@ -7,15 +7,14 @@ import PawsRatingInput from './PawsRatingInput';
 const ReviewList = ({ review }) => {
   const [showEditForm, setShowEditForm] = useState(false);
   const dispatch = useDispatch();
-  const [disabled, setDisabled] = useState(true);
 
   return (
     showEditForm ? (
       <EditReviewForm reviewId={review.id} closeForm={() => setShowEditForm(false)} />
     ) : (
       <>
-        <PawsRatingInput rating={review.rating} disabled={disabled}/>
-        <button onClick={() => {setShowEditForm(true); setDisabled(!disabled)}}>Edit</button>
+        <PawsRatingInput rating={review.rating} disabled={true}/>
+        <button onClick={() => setShowEditForm(true)}>Edit</button>
         <button onClick={() => dispatch(deleteReview(review.id))}>Delete</button>
       </>
     )
